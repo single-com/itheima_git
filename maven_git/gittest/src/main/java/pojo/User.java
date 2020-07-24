7f5e4408fdc87d83adcd645f6dce87f2da3ac3fb
single <single@yeah.com>
2020-07-24 20:23:03 +0800

    firstcommit

---
+++ maven_git/gittest/src/main/java/pojo/User.java
@@ -0,0 +1,26 @@
+package pojo;
+
+/**
+ * @author GaoXG
+ * Date: Create in 2020/7/24 20:19
+ **/
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
